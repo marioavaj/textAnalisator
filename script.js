07ffca0503152bd272d262c5c3fecbcacc8f9364
marioavaj <predaj@predaj-tonerov.sk>
2022-05-12 11:31:15 +0200

    najdlhsie slovo funkcia

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let findedWord = document.getElementById("search").value;
 let resultSearching = document.getElementById("resultSearching");
 let analyse = document.getElementById("analyse");
 
+
 function find() {
   textArea = document.getElementById("textarea").value;
 
@@ -13,11 +14,17 @@ function find() {
   } else resultSearching.innerText = "Hľadané slovo sa nenachadza na stránke ";
 }
 
+
+
 function crypting() {}
 
-function analysing() {
-  textArea = document.getElementById("textarea").value;
 
+
+
+function analysing() {
+    
+  textArea = document.getElementById("textarea").value;
+  let splitt = textArea.split( " ");
   /**dlzka textu */
   let stringLength = textArea.length;
 
@@ -27,21 +34,34 @@ function analysing() {
 
   /**Pocet cisel*/
   let numberCount=0;
-  let splitt = textArea.split( " ");
+  
    for( i =0; i<splitt.length; i++){
     let numbers=parseInt(splitt[i]);
       if (numbers > 0 || numbers < 0){
-      numberCount++;
-      }     
+      numberCount++;     
+      }       
   }
-  console.log(numberCount++);
+
+
+  /**Najdlhsie slovo*/
+  let longerWord=0;
+ let najdlsieSlovovo;
+  
+     for(i =0; i<splitt.length; i++){
+    let lengthOfWord = splitt[i].length;
+    console.log(splitt[i].length)
+    if (lengthOfWord>longerWord){
+        console.log(lengthOfWord + ">" + longerWord)
+        longerWord = splitt[i].length; 
+        najdlsieSlovovo = splitt[i];
+       } 
+       } 
   
 
   analyse.style.backgroundColor = "#ff0000";
   analyse.innerHTML =
     "Celkovy počet znakov v texte aj s medzerami je " + stringLength + "<br />" +
-    "Celkovy počet znakov v texte bez medzier je " +
-    stringlengthNoSpaces +
-    "<br />" +
-    "V texte sa nachádza  " + numberCount + " čísel" ;
+    "Celkovy počet znakov v texte bez medzier je " + stringlengthNoSpaces +"<br />" +
+    "Celkovy počet čísel v texte je " + numberCount +"<br />" +
+    "Najdlhšie slovo v texte je " + najdlsieSlovovo;
 }
